4ff3da6811a9b4de2bad9f4cb7648a73841a048e
orestonce <orestonce@163.com>
2024-08-13 10:56:26 +0800

    iostream

--- ChessClient/main.cpp
+++ ChessClient/main.cpp
@@ -1,6 +1,7 @@
 #include "login.h"
 #include "gamepanel.h"
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[]){
     if(argc >= 2 && QString(argv[1]) == "test-startup")
